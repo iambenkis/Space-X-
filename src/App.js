1a39fda75d7a0e16955e47bef66e48c55d9d4cc7
benjamin kisenge <benkisenge03@gmail.com>
2022-10-04 13:30:12 +0200

    Implement the header

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import {
 import Rocket from './pages/Rocket/Rocket';
 import Mission from './pages/Mission/Mission';
 import Profile from './pages/Profile/Profile';
+import Header from './components/Header/Header';
 
 const App = () => (
   <>
+    <Header />
     <Routes>
       <Route path="/rockets" element={<Rocket />} />
       <Route path="/" element={<Navigate replace to="/rockets" />} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => <></>;
+const Header = () =>
+(<header>
+    <nav>
+        <ul>
+            <li><NavLink to="/">Rockets</NavLink></li>
+            <li><NavLink to="/missions">Missions</NavLink></li>
+            <li><NavLink to="/profile">Profile</NavLink></li>
+        </ul>
+    </nav>
+</header>);
 
 export default Header;
