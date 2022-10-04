5a7066975250a0b802cb7f25a22cb0ab19019c16
skyv26 <dev.aakashv@gmail.com>
2022-10-04 16:02:14 +0530

    Improve the code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <>
-
-    </>
-  );
-}
+const App = () => <></>;
 
 export default App;
