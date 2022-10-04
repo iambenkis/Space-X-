9f00a2544875be8b7fa11e9a4747aae97070d80d
benjamin kisenge <benkisenge03@gmail.com>
2022-10-04 13:31:35 +0200

    Resolve eslint errors

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Header = () =>
-(<header>
+const Header = () => (
+  <header>
     <nav>
-        <ul>
-            <li><NavLink to="/">Rockets</NavLink></li>
-            <li><NavLink to="/missions">Missions</NavLink></li>
-            <li><NavLink to="/profile">Profile</NavLink></li>
-        </ul>
+      <ul>
+        <li><NavLink to="/">Rockets</NavLink></li>
+        <li><NavLink to="/missions">Missions</NavLink></li>
+        <li><NavLink to="/profile">Profile</NavLink></li>
+      </ul>
     </nav>
-</header>);
+  </header>
+);
 
 export default Header;
