d935c66b135f26079b91d1ec4e989124ae563abd
skyv26 <dev.aakashv@gmail.com>
2022-10-07 18:09:21 +0530

    Render the `list` into UI

--- src/pages/Mission/Mission.js
+++ src/pages/Mission/Mission.js
@@ -4,11 +4,38 @@ import { useSelector } from 'react-redux';
 
 const Mission = () => {
   const state = useSelector((state) => state.missions);
-  console.log(state);
-  // const dispatch = useDispatch();
   return (
     <>
-      <p>Mission Page</p>
+      <table className="table">
+        <thead className="tableHeading">
+          <tr className="tableRow">
+            <th className="tableHeading">Mission</th>
+            <th className="tableHeading">Description</th>
+            <th className="tableHeading">Status</th>
+            <th className="tableHeading">{' '}</th>
+          </tr>
+        </thead>
+        <tbody className="tableBody">
+          {
+            state.payload.map((mission) => (
+              <tr key={mission.mission_id} className="tableRow">
+                <td className="tableData">{mission.mission_name}</td>
+                <td className="tableData">{mission.description}</td>
+                <td className="tableData">
+                  {
+                    mission.reserved
+                      ? 'Active Member'
+                      : 'Not A Member'
+                  }
+                </td>
+                <td className="tableData">
+                  <button type="button">Join Missio</button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </>
   );
 };
