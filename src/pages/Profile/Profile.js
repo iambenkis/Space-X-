9ba40f2a7ce991e93b9389df914ba26583e28ade
benjamin kisenge <benkisenge03@gmail.com>
2022-10-07 12:50:32 +0200

    Import  file

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './Profile.css'
 
 const Profile = () => {
   const rocketStore = useSelector(state => state.rockets);
   const reservedRocket = rocketStore.filter(r => r.reserved);
   return(
   <>
-    <h3>My Rockets</h3>
-    <ul>
-      {
-        reservedRocket.map(r =>
-          <li>{r.name}</li>
-        )
-      }
-    </ul>
+    <div className='profile'>
+      <h3>My Rockets</h3>
+      <ul>
+        {
+          reservedRocket.map(r =>
+            <li>{r.name}</li>
+          )
+        }
+      </ul>
+    </div>
   </>
 )};
 
