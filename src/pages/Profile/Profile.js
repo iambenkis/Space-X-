58fcf13c8865ea74bacbb4b9b982dd5cdf4b0a84
benjamin kisenge <benkisenge03@gmail.com>
2022-10-07 09:02:04 +0200

    Get the resserved rockets

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-const Profile = () => (
+const Profile = () => {
+  const rocketStore = useSelector(state => state.rockets);
+  const dispatch = useDispatch();
+  const reservedRocket = rocketStore.filter(r => r.reserved);
+  console.log(reservedRocket,"profile")
+  return(
   <>
     <p>Profile Page</p>
+
   </>
-);
+)};
 
 export default Profile;
