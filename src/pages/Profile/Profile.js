262fd1164c37e57d5955ae455f5a9e12adce7db2
benjamin kisenge <benkisenge03@gmail.com>
2022-10-07 12:25:21 +0200

    Delete unexpected

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux';
 const Profile = () => {
   const rocketStore = useSelector(state => state.rockets);
   const reservedRocket = rocketStore.filter(r => r.reserved);
-  console.log(reservedRocket,"profile")
   return(
   <>
     <h3>My Rockets</h3>
